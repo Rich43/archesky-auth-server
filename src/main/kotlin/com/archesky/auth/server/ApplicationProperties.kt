0fe287717e976e70cf5f6c2e3610ca82b0f1e8ba
Richie Ward <RichieS@GMail.com>
2020-07-17 19:59:14 +0100

    Fix bug from code cleanup

--- src/main/kotlin/com/archesky/auth/server/ApplicationProperties.kt
+++ src/main/kotlin/com/archesky/auth/server/ApplicationProperties.kt
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.PropertySource
 @Configuration
 @PropertySource("classpath:application.properties")
 data class ApplicationProperties (
-        @Value("openid.connect.certificate.url") val certificateURL: String,
-        @Value("openid.connect.issuer") val issuer: String
+        @Value("\${openid.connect.certificate.url}") val certificateURL: String,
+        @Value("\${openid.connect.issuer}") val issuer: String
 )
