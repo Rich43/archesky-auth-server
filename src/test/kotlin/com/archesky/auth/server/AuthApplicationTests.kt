3a4f0da4ca9641519785935ee30d408355d261d1
Richie Ward <RichieS@GMail.com>
2020-07-18 22:52:40 +0100

    Run test on random port

--- src/test/kotlin/com/archesky/auth/server/AuthApplicationTests.kt
+++ src/test/kotlin/com/archesky/auth/server/AuthApplicationTests.kt
@@ -2,8 +2,9 @@ package com.archesky.auth.server
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 
-@SpringBootTest
+@SpringBootTest(webEnvironment=RANDOM_PORT)
 class AuthApplicationTests {
 
 	@Test
